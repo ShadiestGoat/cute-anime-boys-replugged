abbe8f065e26998c9fab4682309fadc97f035b3e
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2022-08-28 06:26:59 +0100

    added cute feature

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const { Plugin } = require('powercord/entities');
+const { getModule } = require("powercord/webpack");
+
+function rand(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 module.exports = class Annoy extends Plugin {
   startPlugin () {
@@ -7,13 +12,19 @@ module.exports = class Annoy extends Plugin {
       description: 'Send a cute anime boy in chat',
       usage: '{c} -p',
       executor: async (args) => {
-        const rand = Math.floor(Math.random() * 100) + 1;
-        const res = await fetch(`https://www.reddit.com/r/cuteanimeboys/top.json?limit=${rand}&t=all`)
-        const resp = await res.json()
-        const url = resp.data.children[rand-1].data.url
+        let url = ""
         let send = true
         if (args.length >= 1) {
           send = args[0] != "-p"
+        }
+        if (rand(1, 1000) != 420) { // nice
+          const r = rand(1, 100)
+          const res = await fetch(`https://www.reddit.com/r/cuteanimeboys/top.json?limit=${rand}&t=all`)
+          const resp = await res.json()
+          url = resp.data.children[r-1].data.url
+        } else {
+          const usr = getModule(["getCurrentUser"], false).getCurrentUser()
+          url = `https://cdn.discordapp.com/avatars/${usr.id}/${usr.avatar}.webp?size=512`
         }
         return {
           send,
