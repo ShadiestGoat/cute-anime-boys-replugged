862f0bd2e64162896350808a0432bbdc2526e192
ShadiestGoat <48590492+ShadiestGoat@users.noreply.github.com>
2022-08-28 12:27:39 +0100

    Added another easter egg

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const { Plugin } = require('powercord/entities');
-const { getModule } = require("powercord/webpack");
+const { getModule, channels } = require("powercord/webpack");
 
 function rand(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+async function fetchReddit() {
+  const r = rand(1, 100)
+  const res = await fetch(`https://www.reddit.com/r/cuteanimeboys/top.json?limit=${r}&t=all`)
+  const resp = await res.json()
+  let url = ""
+  try {
+    url = resp.data.children[r-1].data.url
+  } catch (err) {
+    console.error(resp)
+    console.error(r)
+    console.error(err)
+  }
+  return url
 }
 
 module.exports = class Annoy extends Plugin {
@@ -17,21 +32,24 @@ module.exports = class Annoy extends Plugin {
         if (args.length >= 1) {
           send = args[0] != "-p"
         }
-        if (rand(1, 1000) != 420) { // nice
-          const r = rand(1, 100)
-          const res = await fetch(`https://www.reddit.com/r/cuteanimeboys/top.json?limit=${r}&t=all`)
-          const resp = await res.json()
-          try {
-            url = resp.data.children[r-1].data.url
-          } catch (err) {
-            console.error(resp)
-            console.error(r)
-            console.error(err)
-          }
-        } else {
-          const usr = getModule(["getCurrentUser"], false).getCurrentUser()
-          url = `https://cdn.discordapp.com/avatars/${usr.id}/${usr.avatar}.webp?size=512`
+        
+        switch (rand(1, 1000)) {
+          case 420:
+            const usr = getModule(["getCurrentUser"], false).getCurrentUser()
+            url = `https://cdn.discordapp.com/avatars/${usr.id}/${usr.avatar}.webp?size=512`
+            break
+          case 42:
+            const chan = getModule(["getDMFromUserId"], false).getChannel(channels.getChannelId())
+            if (chan.type == 1) {
+              url = `https://cdn.discordapp.com/avatars/${chan.rawRecipients[0].id}/${chan.rawRecipients[0]}.webp?size=512`
+            } else {
+              url = await fetchReddit()
+            }
+            break
+          default:
+            url = await fetchReddit()
         }
+
         return {
           send,
           result: url
